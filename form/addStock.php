e2117633c686cd5a5658e46b699b375b83bce5cd
dallas <dallas.wty@gmail.com>
2022-12-08 18:22:08 +0900

    index, stock page

---
+++ form/addStock.php
@@ -0,0 +1,11 @@
+<?php
+
+$date = $_GET['date'];
+$name = $_GET['price'];
+$price = $_GET['price'];
+$count = $_GET['count'];
+$category = $_GET['category'];
+
+echo $date.'/'.$name.'/'.$price.'/'.$count.'/'.$category
+
+?>
